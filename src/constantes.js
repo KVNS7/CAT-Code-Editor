148e80eee9b02463ae65b5c37681d249e814d495
Tokaa7 <ksoares@outlook.fr>
2024-05-14 00:31:36 +0200

    indentation

--- src/constantes.js
+++ src/constantes.js
@@ -11,17 +11,17 @@ export const LANGUAGE_VERSIONS = {  // Langages du menu déroulant et leur versi
 const name = "Delbot";  // :)
 
 export const CODE_SNIPPETS = {  // exemples de code pour chaque langage
-  c: `#include <stdio.h>\n\nint main(){\n\n\tchar name[] = "`+ name + `";\n\n\tprintf("Bonjour Mr. %s en C !", name);\n\n\treturn 0;\n\n}`,
+  c: `#include <stdio.h>\n\nint main(){\n\n\tchar name[] = "` + name + `";\n\n\tprintf("Bonjour Mr. %s en C !", name);\n\n\treturn 0;\n\n}`,
   java: `public class HelloWorld {\n\tpublic static void main(String[] args) {\n\t\tString name = "Delbot";\n\t\tSystem.out.println("Bonjour Mr. " + name  + " en Java !");\n\t}\n}\n`,
   python: `def salut(name):\n\tprint("Bonjour Mr. " + name + " en Python !")\n\nsalut("` + name + `")\n`,
   javascript: `function salut(name) {\n\tconsole.log("Bonjour Mr. " + name + " en JavaScript !");\n}\n\nsalut("` + name + `");\n`,
   php: `<?php\n\n$name = '` + name + `';\necho 'Bonjour Mr. '.$name.' en PHP !';\n?>`,
-  csharp: `using System;\n\nnamespace HelloWorld\n{\n\tpublic class Hello { \n\t\tpublic static void Main(string[] args) {\n\t\t\tstring name = "`+ name + `";\n\n\t\t\tConsole.WriteLine("Bonjour Mr. " + name + " en C# !");\n\t\t}\n\t}\n}\n`,
+  csharp: `using System;\n\nnamespace HelloWorld\n{\n\tpublic class Hello { \n\t\tpublic static void Main(string[] args) {\n\t\t\tstring name = "` + name + `";\n\n\t\t\tConsole.WriteLine("Bonjour Mr. " + name + " en C# !");\n\t\t}\n\t}\n}\n`,
   typescript: `type Params = {\n\tname: string;\n}\n\nfunction salut(data: Params) {\n\tconsole.log("Bonjour Mr. " + data.name + " en TypeScript !");\n}\n\nsalut({ name: "` + name + `" });\n`,
 };
 
 export const VS_THEMES = {
-  "Clair" : "vs",
-  "Sombre" : "vs-dark",
-  "Haut Contraste Sombre" : "hc-black",
+  "Clair": "vs",
+  "Sombre": "vs-dark",
+  "Haut Contraste Sombre": "hc-black",
 }
