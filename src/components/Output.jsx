3e83c7547fc876b8382965990610d08f1cb9b49a
KVNS7 <ksoares@outlook.fr>
2024-05-15 20:19:09 +0200

    indentation du code

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,4 +1,4 @@
-import { 
+import {
     Box, Flex,
     Text, Button,
     useToast
@@ -7,7 +7,7 @@ import { executeCode } from "../api";
 import { useState } from "react";
 import { LANGUAGE_VERSIONS } from "../constantes";
 
-const Output = ({content, language}) => {
+const Output = ({ content, language }) => {
 
     const toast = useToast();                                                   // Notification en cas d'erreur
     const [output, setOutput] = useState(null)                                  // Stocke le résultat d'exécution
@@ -15,11 +15,11 @@ const Output = ({content, language}) => {
     const [isError, setIsError] = useState(false)                               // Etat en cas d'erreur
 
     const runCode = async () => {
-        
+
         const supportedLanguages = Object.keys(LANGUAGE_VERSIONS);              // Récupère les langages pris en charge
 
         if (!supportedLanguages.includes(language)) {                           // Si langage du fichier non reconnu, return
-            toast({                                                        
+            toast({
                 title: "Language non reconnu",
                 description: "Aucun langage reconnu pour ce fichier",
                 status: "error",
@@ -32,10 +32,10 @@ const Output = ({content, language}) => {
         }
 
         const sourceCode = content;                                             // Récupère le code depuis l'éditeur
-        if(!sourceCode) return;                                                 // Si vide, return
+        if (!sourceCode) return;                                                // Si vide, return
         try {
             setIsLoading(true)                                                  // Début animation chargement
-            const {run:resultat} = await executeCode(language, sourceCode);     // Execution du code selon le langage choisi et stocke le resultat run dans resultat
+            const { run: resultat } = await executeCode(language, sourceCode);  // Execution du code selon le langage choisi et stocke le resultat run dans resultat
             setOutput(resultat.output.split("\n"));                             // Met le résultat dans output pour l'affichage
             resultat.stderr ? setIsError(true) : setIsError(false);             // Indique si une erreur est survenue en vérifiant la sortie d'erreur
         } catch (error) {
@@ -44,46 +44,46 @@ const Output = ({content, language}) => {
                 title: "Une erreur est survenue au niveau de l'API Piston",
                 description: error.message || "Impossible d'exécuter le code",
                 status: "error",
-                duration : 5000,
-                variant : "left-accent",
+                duration: 5000,
+                variant: "left-accent",
                 position: "bottom-right",
             })
-        }finally{
+        } finally {
             setIsLoading(false);                                                // Fin animation chargement
         }
-    
+
     }
 
-  return (
-    <Box w = "35%">
+    return (
+        <Box w="35%">
 
-        <Flex alignItems = "center" >
-            <Box ml = "auto">
-                {/* Bouton d'exécution */}
-                <Button variant = 'outline' colorScheme = "green" mb = {2} isLoading = {isLoading} onClick = {runCode}> 
-                    Exécuter
-                </Button>
+            <Flex alignItems="center" >
+                <Box ml="auto">
+                    {/* Bouton d'exécution */}
+                    <Button variant='outline' colorScheme="green" mb={2} isLoading={isLoading} onClick={runCode}>
+                        Exécuter
+                    </Button>
+                </Box>
+            </Flex>
+
+
+            <Box
+                height="75vh"
+                p={2}
+                color={isError ? "red.400" : ""}
+                border="1px solid"
+                borderRadius={4}
+                borderColor={isError ? "red.500" : "#333"}
+                mt={12}
+                overflowY="auto"
+                maxHeight="75vh"
+            >
+                {output ?
+                    output.map((line, i) => <Text key={i}>{line}</Text>)             // Affiche le résultat d'exécution ligne par ligne
+                    : 'Cliquez sur exécuter pour voir le résultat de votre code'}   {/* Message par défaut */}
             </Box>
-        </Flex>
-
-
-        <Box 
-            height = "75vh"
-            p = {2}
-            color = {isError ? "red.400" : ""}
-            border = "1px solid"
-            borderRadius = {4}
-            borderColor = {isError ? "red.500" : "#333"}
-            mt={12}
-            overflowY="auto"
-            maxHeight="75vh"
-        >
-            {output ? 
-                output.map((line, i) => <Text key = {i}>{line}</Text>)          // Affiche le résultat d'exécution ligne par ligne
-                : 'Cliquez sur exécuter pour voir le résultat de votre code'}   {/* Message par défaut */}
         </Box>
-    </Box>
-  );
+    );
 };
 
 export default Output
