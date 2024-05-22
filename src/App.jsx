058a74a3e2007d02a432d6375177a5b5f7389ad4
KVNS7 <ksoares@outlook.fr>
2024-05-22 03:38:55 +0200

    ajout de routes vers pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CodeEditor from "./components/CodeEditor";
-
-const BG_ORIGINAL = "#0f0a19"
-const BG_GRIS_FONCE = "#121212"
-const BG_BLEU_CHARBON = "#1E1E2E"
-const BG_GRIS_ARDOISE = "#2C3E50"
-const BG_ANTHRA = "#333644"
-const BG_NOIR_EBENE = "#0D0D0D"
-
+import DockerAdminPage from './components/DockerAdminPage';
 
 function App() {
-  return  <Box minH = "100vh" bg = {BG_GRIS_FONCE} color = "gray.500" px = {6} py = {8}>
-            <CodeEditor/>   {/* Voir CodeEditos.jsx */}
-          </Box>
-
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CodeEditor />} />
+        <Route path="/admin" element={<DockerAdminPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
